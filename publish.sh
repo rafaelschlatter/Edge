aa6b814b7fd651c4f1388428de6eb67bd58618d2
srudaa <sigbjorn.rudaa@raalabs.com>
2021-02-15 15:53:39 +0100

    Added nuget package publish

---
+++ publish.sh
@@ -0,0 +1,9 @@
+
+VERSION=$(git tag --sort=-version:refname | head -1)
+PACKAGEDIR=$PWD/Packages
+dotnet build --configuration Release
+rm -rf $PACKAGEDIR
+dotnet pack --no-build --configuration Release -o $PACKAGEDIR /p:PackageVersion=$VERSION -p:IncludeSymbols=true -p:SymbolPackageFormat=snupkg
+for f in $PACKAGEDIR/*.nupkg; do
+dotnet nuget push --skip-duplicate $f --api-key $NUGET_API_KEY --source https://api.nuget.org/v3/index.json
+done
