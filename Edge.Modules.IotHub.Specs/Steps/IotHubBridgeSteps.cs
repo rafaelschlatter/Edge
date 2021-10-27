84743f7cf0aca4187513a54439930228415eb3fc
Thomas Toekje <Thomas.Toekje@raalabs.com>
2021-10-27 14:36:01 +0200

    Added tests for IotHub module.

---
+++ Edge.Modules.IotHub.Specs/Steps/IotHubBridgeSteps.cs
@@ -0,0 +1,28 @@
+﻿using TechTalk.SpecFlow;
+using BoDi;
+using Microsoft.Azure.Devices.Client;
+using RaaLabs.Edge.Modules.IotHub.Specs.Drivers;
+
+namespace RaaLabs.Edge.Modules.IotHub.Specs.Steps
+{
+    [Binding]
+    class IotHubBridgeSteps
+    {
+        private readonly IObjectContainer _container;
+
+        public IotHubBridgeSteps(IObjectContainer container)
+        {
+            _container = container;
+        }
+
+        [BeforeScenario]
+        public void SetupEventFactories()
+        {
+            _container.RegisterTypeAs<SomeIotHubOutgoingEventInstanceFactory, IEventInstanceFactory<SomeIotHubOutgoingEvent>>();
+            _container.RegisterTypeAs<MessageInstanceFactory, IEventInstanceFactory<Message>>();
+
+            _container.RegisterTypeAs<MessageVerifier, IProducedEventVerifier<Message>>();
+            _container.RegisterTypeAs<SomeIotHubIncomingEventVerifier, IProducedEventVerifier<SomeIotHubIncomingEvent>>();
+        }
+    }
+}
