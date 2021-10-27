a17e7c0d27f89c68eb963a3c75bbf2bc71bfea39
Thomas Toekje <Thomas.Toekje@raalabs.com>
2021-10-27 14:17:28 +0200

    Added tests for Timescaledb module.

---
+++ Edge.Modules.Timescaledb.Specs/Steps/TimescaledbBridgeSteps.cs
@@ -0,0 +1,27 @@
+﻿using TechTalk.SpecFlow;
+using BoDi;
+using RaaLabs.Edge.Modules.Timescaledb.Specs.Drivers;
+
+namespace RaaLabs.Edge.Modules.Timescaledb.Specs.Steps
+{
+    [Binding]
+    class TimescaledbBridgeSteps
+    {
+        private readonly IObjectContainer _container;
+
+        public TimescaledbBridgeSteps(IObjectContainer container)
+        {
+            _container = container;
+        }
+
+        [BeforeScenario]
+        public void SetupEventFactories()
+        {
+            _container.RegisterTypeAs<SomeTimescaledbEventInstanceFactory, IEventInstanceFactory<SomeTimescaledbEvent>>();
+            _container.RegisterTypeAs<AnotherTimescaledbEventInstanceFactory, IEventInstanceFactory<AnotherTimescaledbEvent>>();
+            _container.RegisterTypeAs<ThirdTimescaledbEventInstanceFactory, IEventInstanceFactory<ThirdTimescaledbEvent>>();
+
+            _container.RegisterTypeAs<ObjectEventVerifier, IProducedEventVerifier<object>>();
+        }
+    }
+}
